f540bb3cc7fc9a9265b6940e580c43a8f6b386f0
Andre Thomas <drethedevjs@gmail.com>
2024-11-29 16:50:23 -0500

    initial

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+
+export default function Header() {
+  return (
+    <nav>
+      <ul className="nav-links">
+        <li>Home</li>
+        <li>About</li>
+        <li>Services</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  )
+}
---
+++ src/components/Hero.tsx
@@ -0,0 +1,11 @@
+
+export default function Hero() {
+  return (
+    <div className="relative">
+      <div className="hero-img" style={{backgroundImage: 'url("/laptop-hero.jpg")'}}>
+        <h1>Web Development Services</h1>
+        <p>Building Custom Web Applications for Small Businesses</p>
+      </div>
+    </div>
+  )
+}
