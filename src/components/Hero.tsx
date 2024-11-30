dad499ad29cf49b858c24684cac3385a872156b1
Andre Thomas <drethedevjs@gmail.com>
2024-11-30 05:36:31 -0500

    Updated Hero section to be minimalist

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,11 @@
+import { ComputerDesktopIcon } from "@heroicons/react/24/outline"
 
 export default function Hero() {
   return (
-    <div className="relative">
-      <div className="hero-img" style={{backgroundImage: 'url("/laptop-hero.jpg")'}}>
-        <h1>Web Development Services</h1>
-        <p>Building Custom Web Applications for Small Businesses</p>
-      </div>
+    <div className="hero-section">
+        <ComputerDesktopIcon className="computer-desktop-icon" />
+      <h1>Web Development Services</h1>
+      <p>Building Custom Web Applications for Small Businesses</p>
     </div>
   )
 }
