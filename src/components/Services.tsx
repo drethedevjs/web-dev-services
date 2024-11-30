f145fd1582d33215c306a0b0e0a40f11bf6720f0
Andre Thomas <drethedevjs@gmail.com>
2024-11-30 05:49:20 -0500

    Jazzed up the services section and added it's own header.

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -4,18 +4,23 @@ export default function Services() {
   return (
     <section>
       <div className="service-pane">
-        {
-          services.map(s => {
-            const IconComponent = s.icon;
-            return (
-              <div className="service-offering" key={s.id}>
-                <IconComponent className="size-20 font-thin" />
-                <h3>{s.name}</h3>
-                <p>{s.description}</p>
-              </div>
-            )
-          })
-        }
+
+        <h2>Service Offerings</h2>
+        <p>We offer the best-in-class web services to small businesses.</p>
+        <div className="offerings">
+          {
+            services.map(s => {
+              const IconComponent = s.icon;
+              return (
+                <div className="service-offering" key={s.id}>
+                  <IconComponent className="size-20 rotate-0 hover:rotate-6 transition-transform" />
+                  <h3>{s.name}</h3>
+                  <p className="description">{s.description}</p>
+                </div>
+              )
+            })
+          }
+        </div>
       </div>
     </section>
   )
