50646014c23e694a2ec31eb084a991180ee65a87
Andre Thomas <drethedevjs@gmail.com>
2024-11-30 22:53:20 -0500

    Added footer to layout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
@@ -34,6 +35,7 @@ export default function RootLayout({
       >
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
