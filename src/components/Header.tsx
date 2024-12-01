2861d0f7a551713a23195226c6524d31b0d23fb8
Andre Thomas <drethedevjs@gmail.com>
2024-11-30 22:53:40 -0500

    Made pages responsive.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,49 @@
 'use client'
-import { useRouter } from "next/navigation"
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function Header() {
-  const router = useRouter()
+  const router = useRouter();
+  const [showMenu, setShowMenu] = useState<boolean>(false);
+
+  const navigate = (page: string) => {
+    router.push(page);
+    toggleMenu();
+  }
+  const toggleMenu = () => {
+    const menuWillShow = showMenu;
+    setShowMenu(!menuWillShow)
+  }
+
   return (
     <nav>
       <ul className="nav-links">
         <li>
-          <button onClick={() => router.push("/")}>Home</button>
+          <button onClick={() => navigate("/")}>Home</button>
         </li>
         <li>
-          <button onClick={() => router.push("/about")}>About</button>
+          <button onClick={() => navigate("/about")}>About</button>
         </li>
         <li>
-          <button onClick={() => router.push("/services")}>Services</button>
+          <button onClick={() => navigate("/services")}>Services</button>
         </li>
         <li>
-          <button onClick={() => router.push("/contact")}>Contact</button>
+          <button onClick={() => navigate("/contact")}>Contact</button>
+        </li>
+      </ul>
+      <button className="uppercase text-2xl py-3 w-full text-center border-2 border-secondary" onClick={toggleMenu}>Menu</button>
+      <ul className={`mobile-nav-links ${showMenu ? 'flex flex-col' : 'hidden'}`}>
+        <li>
+          <button onClick={() => navigate("/")}>Home</button>
+        </li>
+        <li>
+          <button onClick={() => navigate("/about")}>About</button>
+        </li>
+        <li>
+          <button onClick={() => navigate("/services")}>Services</button>
+        </li>
+        <li>
+          <button onClick={() => navigate("/contact")}>Contact</button>
         </li>
       </ul>
     </nav>
