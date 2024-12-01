944156627fb4015e528c9d4d968eba832a92e8b5
Andre Thomas <drethedevjs@gmail.com>
2024-11-30 22:53:05 -0500

    Added Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer>
+      &copy; 2024 <span className="text-secondary">Web Development Services</span>. All rights reserved.
+    </footer>
+  )
+}
