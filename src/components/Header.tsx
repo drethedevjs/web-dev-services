43b04c98338463206b550695dbca4ed156b05216
Andre Thomas <drethedevjs@gmail.com>
2024-12-02 10:09:07 -0500

    Hid mobile menu button when menu is showing and added a close button.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 'use client'
+import { XCircleIcon } from "@heroicons/react/16/solid";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
@@ -33,21 +34,27 @@ export default function Header() {
           <button onClick={() => navigate("/contact")}>Contact</button>
         </li>
       </ul>
-      <button className="md:hidden uppercase text-2xl py-3 w-full text-center border-2 border-secondary" onClick={toggleMenu}>Menu</button>
-      <ul className={`mobile-nav-links ${showMenu ? 'flex flex-col' : 'hidden'}`}>
-        <li>
-          <button onClick={() => navigate("/", true)}>Home</button>
-        </li>
-        <li>
-          <button onClick={() => navigate("/about", true)}>About</button>
-        </li>
-        <li>
-          <button onClick={() => navigate("/services", true)}>Services</button>
-        </li>
-        <li>
-          <button onClick={() => navigate("/contact", true)}>Contact</button>
-        </li>
-      </ul>
+
+      <button className={`mobile-menu-btn ${showMenu ? 'hidden' : ''}`} onClick={toggleMenu} >Menu</button>
+      <div className={`mobile-nav-container ${showMenu ? 'flex flex-col' : 'hidden'}`}>
+        <div className="place-items-center">
+          <XCircleIcon className="size-12 m-5 text-primary" onClick={toggleMenu} />
+        </div>
+        <ul className="mobile-nav-links">
+          <li>
+            <button onClick={() => navigate("/", true)}>Home</button>
+          </li>
+          <li>
+            <button onClick={() => navigate("/about", true)}>About</button>
+          </li>
+          <li>
+            <button onClick={() => navigate("/services", true)}>Services</button>
+          </li>
+          <li>
+            <button onClick={() => navigate("/contact", true)}>Contact</button>
+          </li>
+        </ul>
+      </div>
     </nav>
   )
 }
