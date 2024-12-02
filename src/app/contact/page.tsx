94d8a1411bce8f0d37042fa0fbd16fe8acb2d15e
Andre Thomas <drethedevjs@gmail.com>
2024-12-02 14:47:03 -0500

    Added button to contact form and enlarged label text.

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -5,12 +5,12 @@ export default function Contact() {
     <div className="wds-container page">
       <h1>Contact</h1>
       <form>
-        <div className="grid grid-cols-2 gap-4">
-          <div>
+        <div className="grid md:grid-cols-2 grid-cols-1 gap-4">
+          <div className="md:col-span-1 col-span-2">
             <label htmlFor="first-name">First Name</label>
             <input id="first-name" name="first-name" required />
           </div>
-          <div>
+          <div className="md:col-span-1 col-span-2">
             <label htmlFor="last-name">Last Name</label>
             <input id="last-name" name="last-name" required />
           </div>
@@ -23,6 +23,7 @@ export default function Contact() {
             <textarea id="message" name="message" minLength={15} rows={6} />
           </div>
         </div>
+        <button className="send-btn">Send</button>
       </form>
     </div>
   )
