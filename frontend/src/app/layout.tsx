cefc9695be881bf2c3f59c770e2809a437913caf
Andre Thomas <drethedevjs@gmail.com>
2026-01-21 08:31:16 -0500

    Refactored, add gitignore, and removed font reference.

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,22 +1,10 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-
 export const metadata: Metadata = {
-  title: "CSRA Custom Websites",
+  title: "CSRA Custom Websites and Web Applications",
   description: "Building Custom Web Applications for Small Businesses",
   authors: [{ name: "Andre Thomas", url: "https://csra-web-services.vercel.app" }],
   generator: "Next.js",
@@ -27,13 +15,15 @@ export const metadata: Metadata = {
     title: "CSRA Web Services",
     description: "Building custom websites for small businesses.",
     siteName: "CSRA Web Services",
-    images: [{
-      url: "https://csra-web-services.vercel.app/csra-web-dev-screensht.png", // Corrected image URL
-      width: 1920,
-      height: 1080,
-      alt: "Screenshot of CSRA Web Services"
-    }],
-   }
+    images: [
+      {
+        url: "https://csra-web-services.vercel.app/csra-web-dev-screensht.png", // Corrected image URL
+        width: 1920,
+        height: 1080,
+        alt: "Screenshot of CSRA Web Services",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -43,14 +33,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
         <div className="flex flex-col min-h-screen">
           <Header />
-          <main className="flex-grow">
-            {children}
-          </main>
+          <main className="flex-grow">{children}</main>
           <Footer />
         </div>
       </body>
