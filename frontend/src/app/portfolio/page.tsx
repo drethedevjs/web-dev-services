a7ddc629abd1455eec71b1067e07ff72a220ece1
Andre Thomas <drethedevjs@gmail.com>
2026-01-21 14:01:04 -0500

    Removing flowbite.

--- frontend/src/app/portfolio/page.tsx
+++ frontend/src/app/portfolio/page.tsx
@@ -1,6 +1,3 @@
-import websites from "@/data/websites";
-import { Card } from "flowbite-react";
-
 const Portfolio = () => {
   return (
     <div className="wds-container page">
@@ -11,7 +8,8 @@ const Portfolio = () => {
       </p>
 
       <div className="grid xl:grid-cols-3 sm:grid-cols-2 gap-4 mb-5">
-        {websites.map((w, idx) => {
+        Re-add the card!
+        {/* {websites.map((w, idx) => {
           return (
             <Card key={idx} className="max-w-sm" imgAlt="" imgSrc={w.imgSrc}>
               <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
@@ -27,7 +25,7 @@ const Portfolio = () => {
               </a>
             </Card>
           );
-        })}
+        })}*/}
       </div>
     </div>
   );
