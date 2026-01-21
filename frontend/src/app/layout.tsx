fe84714351d1cbc622cc46bfc505b2f1bbfcabe5
Andre Thomas <drethedevjs@gmail.com>
2026-01-21 14:35:47 -0500

    Removed the remaining flowbite and upgraded tailwind to v4.

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
       <body>
         <div className="flex flex-col min-h-screen">
           <Header />
-          <main className="flex-grow">{children}</main>
+          <main className="grow">{children}</main>
           <Footer />
         </div>
       </body>
