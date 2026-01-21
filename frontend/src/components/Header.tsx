0a391c6461e1e32790a82f4dc5b4a35415f5f52d
Andre Thomas <drethedevjs@gmail.com>
2026-01-21 16:01:01 -0500

    Updated header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,17 +1,10 @@
 "use client";
 import { headerLinks } from "@/data/headerLinks";
-import { XCircleIcon } from "@heroicons/react/16/solid";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 import { useState } from "react";
 
 export default function Header() {
-  const router = useRouter();
   const [showMenu, setShowMenu] = useState<boolean>(false);
-
-  const navigate = (page: string, isMobile: boolean = false) => {
-    router.push(page);
-    if (isMobile) toggleMenu();
-  };
 
   const toggleMenu = () => {
     const menuWillShow = showMenu;
@@ -19,34 +12,91 @@ export default function Header() {
   };
 
   return (
-    <nav>
-      <ul className="nav-links">
-        {headerLinks.map((link, idx) =>
-          link.active ? (
-            <li key={idx}>
-              <button onClick={() => navigate(link.path)}>{link.title}</button>
-            </li>
-          ) : null,
-        )}
-      </ul>
+    <header className="mb-10 flex-col bg-white border-b border-gray-200 dark:bg-neutral-800 dark:border-neutral-700 flex flex-wrap md:justify-start md:flex-nowrap z-50 w-full">
+      <nav className="relative max-w-[85rem] w-full md:flex md:items-center md:justify-between md:gap-3 mx-auto px-4 sm:px-6 lg:px-8 py-2 dark:bg-neutral-900">
+        {/* Logo w/ Collapse Button */}
+        <div className="flex items-center justify-between">
+          <Link
+            className="flex-none font-semibold text-3xl text-black focus:outline-hidden focus:opacity-80 dark:text-white"
+            href="/"
+            aria-label="Brand"
+          >
+            Honeystack
+          </Link>
 
-      <button className={`mobile-menu-btn ${showMenu ? "hidden" : ""}`} onClick={toggleMenu}>
-        Menu
-      </button>
-      <div className={`mobile-nav-container ${showMenu ? "flex flex-col" : "hidden"}`}>
-        <div className="place-items-center">
-          <XCircleIcon className="size-12 m-5" onClick={toggleMenu} />
+          {/* Collapse Button */}
+          <div className="md:hidden">
+            <button
+              type="button"
+              className="hs-collapse-toggle relative size-9 flex justify-center items-center text-sm font-semibold rounded-lg border border-gray-200 text-gray-800 hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:border-neutral-700 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
+              id="hs-header-classic-collapse"
+              aria-expanded="false"
+              aria-controls="hs-header-classic"
+              aria-label="Toggle navigation"
+              data-hs-collapse="#hs-header-classic"
+            >
+              <svg
+                className="hs-collapse-open:hidden size-4"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <line x1="3" x2="21" y1="6" y2="6" />
+                <line x1="3" x2="21" y1="12" y2="12" />
+                <line x1="3" x2="21" y1="18" y2="18" />
+              </svg>
+              <svg
+                className="hs-collapse-open:block shrink-0 hidden size-4"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="M18 6 6 18" />
+                <path d="m6 6 12 12" />
+              </svg>
+              <span className="sr-only">Toggle navigation</span>
+            </button>
+          </div>
+          {/* End Collapse Button */}
         </div>
-        <ul className="mobile-nav-links">
-          {headerLinks.map((link, idx) =>
-            link.active ? (
-              <li key={idx}>
-                <button onClick={() => navigate(link.path)}>{link.title}</button>
-              </li>
-            ) : null,
-          )}
-        </ul>
-      </div>
-    </nav>
+        {/* End Logo w/ Collapse Button */}
+
+        {/* Collapse */}
+        <div
+          id="hs-header-classic"
+          className="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow md:block"
+          aria-labelledby="hs-header-classic-collapse"
+        >
+          <div className="overflow-hidden overflow-y-auto max-h-[75vh] [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-neutral-700 dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500">
+            <div className="py-2 md:py-0 flex flex-col md:flex-row md:items-center md:justify-end gap-0.5 md:gap-1">
+              {headerLinks.map((link, idx) =>
+                link.active ? (
+                  <Link
+                    key={idx}
+                    className="p-2 flex items-center text-xl text-gray-800 hover:text-gray-500 focus:outline-hidden focus:text-gray-500 dark:text-neutral-200 dark:hover:text-neutral-500 dark:focus:text-neutral-500"
+                    href={link.path}
+                  >
+                    {link.title}
+                  </Link>
+                ) : null,
+              )}
+            </div>
+          </div>
+        </div>
+        {/* End Collapse */}
+      </nav>
+    </header>
   );
 }
