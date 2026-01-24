122646c594c1aee6b381f594ff9acc1ddda06a2b
Andre Thomas <drethedevjs@gmail.com>
2026-01-24 09:59:26 -0500

    Updated text color and name in footer.

--- frontend/src/components/CallToAction.tsx
+++ frontend/src/components/CallToAction.tsx
@@ -1,25 +1,26 @@
-'use client'
+"use client";
 import ICallToActionProps from "@/interfaces/ICallToAction";
 import { PhoneArrowDownLeftIcon } from "@heroicons/react/16/solid";
 import { useRouter } from "next/navigation";
 
 export default function CallToAction({ title, icon, buttonText }: ICallToActionProps) {
   const HeroIcon = {
-    "phone" : PhoneArrowDownLeftIcon
-  }[icon]
+    phone: PhoneArrowDownLeftIcon,
+  }[icon];
 
   const router = useRouter();
   return (
     <div className="contact-call-to-action">
       <HeroIcon className="size-32 rotate-0 hover:-rotate-6 transition-transform mb-10 text-secondary" />
       <h2>{title}</h2>
-      <p>
-        Whether you're looking to build a brand-new website or need help improving 
-        your current one, we're here to help! Our team specializes in custom web 
-        development that's tailored to your business needs. Reach out today to get 
-        started and let's make your vision a reality!
+      <p className="text-eggshell">
+        Whether you're looking to build a brand-new website or need help improving your current one,
+        we're here to help! Our team specializes in custom web development that's tailored to your
+        business needs. Reach out today to get started and let's make your vision a reality!
       </p>
-      <button className="cta-btn" onClick={() => router.push("/contact")}>{ buttonText }</button>
+      <button className="cta-btn" onClick={() => router.push("/contact")}>
+        {buttonText}
+      </button>
     </div>
-  )
+  );
 }
--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,8 +1,9 @@
 export default function Footer() {
-  const currentYear:number = new Date().getFullYear();
+  const currentYear: number = new Date().getFullYear();
   return (
     <footer>
-      &copy; 2024 - { currentYear } <span className="text-secondary">Web Development Services</span>. All rights reserved.
+      &copy; 2024 - {currentYear} <span className="text-secondary">Honeystack</span>. All rights
+      reserved.
     </footer>
-  )
+  );
 }
